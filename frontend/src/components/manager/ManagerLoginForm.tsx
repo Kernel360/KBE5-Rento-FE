20425557ca1f40f918f56eb845fb54f6de27610a
soyun <ehouse16@naver.com>
2025-06-09 18:58:55 +0900

    api 변경

--- frontend/src/components/manager/ManagerLoginForm.tsx
+++ frontend/src/components/manager/ManagerLoginForm.tsx
@@ -15,7 +15,7 @@ const ManagerLoginForm: React.FC = () => {
     setError('');
     
     try {
-      const res = await fetch('/api/managers/login', {
+      const res = await fetch('http://api.rento.world/api/managers/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ 
